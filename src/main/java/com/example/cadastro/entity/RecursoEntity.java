5300b4db598460e70c3794a6b32dc1779cc6482d
Philco <Philco@Philco-PC>
2023-12-04 13:30:37 -0300

    atualização do projeto

---
+++ src/main/java/com/example/cadastro/entity/RecursoEntity.java
@@ -0,0 +1,44 @@
+package com.example.cadastro.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter
+@Entity
+@Table(name="SY_RECURSOS")
+public class RecursoEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false)
+	private String chave;
+	@Override
+	public int hashCode() {
+		return Objects.hash(chave, id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecursoEntity other = (RecursoEntity) obj;
+		return Objects.equals(chave, other.chave) && Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/cadastro/repository/RecursoRepository.java
@@ -0,0 +1,9 @@
+package com.example.cadastro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.cadastro.entity.RecursoEntity;
+
+public interface RecursoRepository extends JpaRepository<RecursoEntity, Long>{
+
+}
